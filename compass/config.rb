7fe7dbebaf1f0755bd8161e273e856f9165c3ecb
Brian Speight <bdunk86@msn.com>
2014-02-16 19:48:41 -0800

    adding susy and changing the file path from 'scripts' to 'javascripts'

--- compass/config.rb
+++ compass/config.rb
@@ -2,7 +2,8 @@
 
 # We also support plugins and frameworks, please read the docs http://docs.mixture.io/preprocessors#compass
 # Susy (susy.oddbird.net) is available in Mixture, just uncomment to require and use
-# require 'susy'
+require 'susy'
+
 project_path = File.expand_path("..",File.dirname(__FILE__))
 
 # Important! change the paths below to match your project setup
@@ -13,7 +14,7 @@ sass_dir = "public/styles/sass" # update to the path of your sass files.
 
 images_dir = "public/images" # update to the path of your image files.
 
-javascripts_dir = "public/scripts" # update to the path of your script files.
+javascripts_dir = "public/javascripts" # update to the path of your script files.
 
 line_comments = false
 
